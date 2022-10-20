ea14231f111005a4632dc6eaaa9682c6932cde87
jomina-jolly <jominajolly@gmail.com>
2022-10-20 10:55:52 +0530

    get api for stores

--- app.py
+++ app.py
@@ -2,3 +2,22 @@ from flask import Flask
 
 app = Flask(__name__)
 
+stores = [
+    {
+        "name" : "mystore_1",
+        "items" : [
+            {
+                "name": "Chair",
+                "price": 15.99
+            },
+            {
+              "name": "Table",
+              "price": 20.99
+            }
+        ]
+    }
+]
+
+@app.get("/stores")
+def get_stores():
+    return {"stores": stores}
